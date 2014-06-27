eaede751be6481b43d95204e5a974ace457accef
omniaura <omniaura5@gmail.com>
2014-06-26 22:49:54 -0700

    added test cases 1 and 2

---
+++ hw/p1/test_01.cpp
@@ -0,0 +1,14 @@
+#include "Game.h"
+#include "Game.h"
+#include "Colosseum.h"
+#include "Colosseum.h"
+#include "History.h"
+#include "History.h"
+#include "Player.h"
+#include "Player.h"
+#include "Villain.h"
+#include "Villain.h"
+#include "globals.h"
+#include "globals.h"
+int main()
+{}
