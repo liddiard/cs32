8fedc7f073ad8f470868754e4a6706f6676d953a
omniaura <omniaura5@gmail.com>
2014-06-28 00:16:18 -0700

    skeleton implementation for History

--- hw/p1/History.cpp
+++ hw/p1/History.cpp
@@ -1,3 +1,17 @@
 #include "History.h"
 
+History::History(int nRows, int nCols)
+{
+    m_nRows = nRows;
+    m_nCols = nCols;    
+}
 
+bool History::record(int r, int c)
+{
+    
+}
+
+void History::display() const
+{
+    
+}
--- hw/p1/History.h
+++ hw/p1/History.h
@@ -1,12 +1,24 @@
 #ifndef HISTORY_INCLUDED
 #define HISTORY_INCLUDED
 
+#include <vector>
+
+struct Coordinate
+{
+    int x;
+    int y;
+};
+
 class History
 {
     public:
-    History(int nRows, int nCols);
-    bool record(int r, int c);
-    void display() const;
+        History(int nRows, int nCols);
+        bool record(int r, int c);
+        void display() const;
+    private:
+        int m_nRows;
+        int m_nCols;
+        std::vector<Coordinate> history;
 };
 
 #endif
