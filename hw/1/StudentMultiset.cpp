77fbef4922e213bbf2fb4c1574074e660ac31db3
omniaura <omniaura5@gmail.com>
2014-07-03 16:47:54 -0700

    implemented StudentMultiset (untested)

---
+++ hw/1/StudentMultiset.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "StudentMultiset.h"
+
+StudentMultiset::StudentMultiset() {
+    this->ids_size = 0;    
+}
+
+bool StudentMultiset::add(unsigned long id)
+{
+    id_preexisting = this->submissions.contains(id);
+    if (this->submissions.insert(id)) // id was successfully inserted
+    {
+        if (!id_preexisting)
+        {
+            this->ids[ids_size] = id;
+            ids_size++;
+        } 
+        return true;
+    }
+    else return false;
+}
+
+int StudentMultiset::size() const
+{
+    return this->submissions.size();
+}
+
+void StudentMultiset::print() const
+{
+    for (int i = 0; i < ids_size; i++)
+    {
+        std::cout << this->ids[i] << std::endl; 
+    }
+}
