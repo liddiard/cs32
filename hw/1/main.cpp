7539ab07f12debb5cfcb0657af7552aa9e4bac74
omniaura <omniaura5@gmail.com>
2014-07-03 14:55:57 -0700

    trying a test case (passes)

--- hw/1/main.cpp
+++ hw/1/main.cpp
@@ -1,6 +1,19 @@
 #include "Multiset.h"
+#include <iostream>
+#include <cassert>
+using namespace std;
 
 int main()
 {
-    return 0;
+    Multiset ms;
+    assert(ms.empty());
+    string x = "dill";
+    assert(ms.get(0, x) == 0  &&  x == "dill");  // x unchanged by get failure
+    assert( ! ms.contains("tamarind"));
+    ms.insert("tamarind");
+    ms.insert("tamarind");
+    assert(ms.size() == 2  &&  ms.uniqueSize() == 1);
+    assert(ms.get(1, x) == 0  &&  x == "dill");  // x unchanged by get failure
+    assert(ms.get(0, x) == 2  &&  x == "tamarind");
+    cout << "Passed all tests" << endl;
 }
