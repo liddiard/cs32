e59665240b680cccd9b5917d7109f4b3b819fcd8
omniaura <omniaura5@gmail.com>
2014-07-09 02:32:56 -0700

    fixed split file template typename issue with Stack implementaiton

--- proj/2/Stack.h
+++ proj/2/Stack.h
@@ -2,6 +2,18 @@
 #define STACK_INCLUDED
 
 template <typename T>
+struct StackNode
+{
+    T data;
+    StackNode* next;
+    StackNode (T data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+template <typename T>
 class Stack
 {
 public:
@@ -12,20 +24,53 @@ public:
 	bool empty() const;
 
 private:
-    template <typename U>
-    struct StackNode
-    {
-        U data;
-        StackNode* next;
-        StackNode (U data)
-        {
-            this->data = data;
-            this->next = nullptr;
-        }
-    };
-
 	StackNode<T> * head;
 };
+
+
+template <typename T>
+Stack<T>::Stack()
+{
+    this->head = nullptr;
+}
+
+template <typename T>
+void Stack<T>::push(T x)
+{
+    StackNode<T>* to_add = new StackNode<T>(x);
+    if (this->head == nullptr)
+        this->head = to_add;
+    else
+    {
+        to_add->next = this->head;
+        this->head = to_add;
+    }
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    if (this->head == nullptr) // the stack is empty
+        return;
+    else
+    {
+        StackNode<T>* new_head = this->head->next;
+        delete this->head;
+        this->head = new_head;
+    }
+}
+
+template <typename T>
+T Stack<T>::top() const
+{
+    return this->head->data; // WARNING: will segfault if stack is empty`
+}
+
+template <typename T>
+bool Stack<T>::empty() const
+{
+    return (this->head == nullptr);
+}
 
 #endif
 
--- proj/2/test_u01.cpp
+++ proj/2/test_u01.cpp
@@ -6,7 +6,12 @@ using namespace std;
 
 int main()
 {
-    Stack<string> t_stack;
-    t_stack.push("hello");
-    cout << t_stack.top();   
+    Stack<string>* t_stack = new Stack<string>;
+    t_stack->push("foo");
+    t_stack->push("bar");
+    if (!t_stack->empty()) cout << t_stack->top();   
+    t_stack->pop();
+    if (!t_stack->empty()) cout << t_stack->top();   
+    t_stack->pop();
+    if (!t_stack->empty()) cout << t_stack->top();   
 }
