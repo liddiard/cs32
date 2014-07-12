35cd59300d0d027936a7d247fe608e63c6b8ee4c
omniaura <omniaura5@gmail.com>
2014-07-11 21:53:57 -0700

    we don't need to check bounds in GOTO anymore because ProgramState::setCounter handles it now

--- proj/2/GotoStatement.cpp
+++ proj/2/GotoStatement.cpp
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <iostream>
 #include "GotoStatement.h"
 using namespace std;
 
@@ -9,11 +7,5 @@ GotoStatement::GotoStatement(int line)
 
 void GotoStatement::execute(ProgramState * state, ostream &outf)
 {
-    if (state->isInBounds(this->m_line))
-        state->setCounter(this->m_line);
-    else
-    {
-        cerr << "Illegal jump instruction";
-        exit(1);
-    }
+    state->setCounter(this->m_line);
 }
