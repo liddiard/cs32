b639780030671db56a4917540a5509d4e28910eb
omniaura <omniaura5@gmail.com>
2014-07-11 21:54:39 -0700

    fixed operand comparison in IF statement

--- proj/2/IfStatement.cpp
+++ proj/2/IfStatement.cpp
@@ -1,34 +1,37 @@
+#include <string>
 #include "IfStatement.h"
 using namespace std;
 
-IfStatement::IfStatement(char var, char op, int compare, int dest)
+IfStatement::IfStatement(char var, string op, int compare, int dest)
     : m_variableName( var ), m_op ( op ), m_comparison ( compare ), 
       m_destination( dest )
 {}
 
 void IfStatement::execute(ProgramState * state, ostream &outf)
 {
-    int val = state->getVariable(var);
+    int val = state->getVariable(this->m_variableName);
     int comparison = this->m_comparison;
-    statement_is_true = this->checkCondition(val, this->m_op, comparison);
+    bool statement_is_true = this->checkCondition(val, this->m_op, comparison);
     if (statement_is_true)
     	state->setCounter(this->m_destination);
     else state->incrementCounter();
 }
 
-bool IfStatement::checkCondition(int first_operand, char op, int second_operand)
+bool IfStatement::checkCondition(int first_operand, string op, int second_operand)
 {
-	switch(op)
-	{
-		case '=':
-			return (first_operand == second_operand);
-		case '>':
-			return (first_operand > second_operand);
-		case '<':
-			return (first_operand < second_operand);
-		case '>=':
-			return (first_operand >= second_operand);
-		case '<=':
-			return (first_operand <= second_operand);
-	}
+	if (op == "=")
+		return (first_operand == second_operand);
+	else if (op == "<>")
+		return (first_operand != second_operand);
+	else if (op == ">")
+		return (first_operand > second_operand);
+	else if (op == "<")
+		return (first_operand < second_operand);
+	else if (op == ">=")
+		return (first_operand >= second_operand);
+	else if (op == "<=")
+		return (first_operand <= second_operand);
+	else
+		return false;
+	// TODO: default case for invalid operator
 }
--- proj/2/IfStatement.h
+++ proj/2/IfStatement.h
@@ -1,21 +1,22 @@
 #ifndef IF_STATEMENT_INCLUDED
 #define IF_STATEMENT_INCLUDED
 
+#include <string>
 #include "Statement.h"
 #include "ProgramState.h"
 
 class IfStatement : public Statement
 {
 public:
-    IfStatement(char var, char op, int compare, int dest);
+    IfStatement(char var, std::string op, int compare, int dest);
     virtual void execute(ProgramState * state, std::ostream &outf);
 
 private:
     char m_variableName;
-    char m_op;
+    std::string m_op;
     int m_comparison;
     int m_destination;
-    bool checkCondition(int first_operand, char op, int second_operand);
+    bool checkCondition(int first_operand, std::string op, int second_operand);
 };
 
 #endif
