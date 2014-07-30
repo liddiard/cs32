64503950a290bbeee282ba0fadf6acaea33d838c
omniaura <omniaura5@gmail.com>
2014-07-30 11:35:39 -0700

    changed Piece method name

--- proj/3/Piece.cpp
+++ proj/3/Piece.cpp
@@ -1,6 +1,8 @@
+#include "Piece.h"
+
 Piece::Piece(){};
 
-void Piece::display()
+void Piece::displayAtPosition(int x, int y)
 {
-	
+    // for each character in each row, move the cursor there and print the character
 }
