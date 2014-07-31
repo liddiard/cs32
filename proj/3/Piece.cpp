f0e76b97ff94933dcf85e094667ce502c6d40620
omniaura <omniaura5@gmail.com>
2014-07-30 22:12:02 -0700

    display Piece, constructor for IPiece

--- proj/3/Piece.cpp
+++ proj/3/Piece.cpp
@@ -1,8 +1,26 @@
 #include "Piece.h"
 
-Piece::Piece(){};
+Piece::Piece() : m_screen(scr) {}
 
-void Piece::displayAtPosition(int x, int y)
+void Piece::displayAtPosition(int x_offset, int y_offset)
 {
     // for each character in each row, move the cursor there and print the character
+    for (int i = 0; i < PIECE_HEIGHT; i++) // for each row of the piece
+    {
+    	for (int j = 0; j < PIECE_WIDTH; j++)
+    	{
+    		m_screen->gotoXY(x_offset + j, y_offset + i);
+    		m_screen->printChar(m_piece[i][j]);
+    	}
+    }
+}
+
+IPiece::IPiece()
+{
+	m_piece = {
+		{' ', ' ', ' ', ' '},
+		{'#', '#', '#', '#'},
+		{' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' '}
+	}
 }
--- proj/3/Piece.h
+++ proj/3/Piece.h
@@ -3,18 +3,27 @@
 
 #include "globals.h"
 
+class Screen;
+
 class Piece
 {
-	public:
-		Piece();
-		virtual void displayAtPosition(int x, int y);
-		virtual void fallOne();
-		virtual void fallAll();
-		virtual void rotateClockwise();
-		virtual void rotateCounterclockwise();
+public:
+	Piece();
+	void displayAtPosition(int x_offset, int y_offset);
+	// void fallOne();
+	// void fallAll();
+	// void rotateClockwise();
+	// void rotateCounterclockwise();
 
-	private:
-		char m_piece[PIECE_HEIGHT][PIECE_WIDTH];
+private:
+	char m_piece[PIECE_HEIGHT][PIECE_WIDTH];
+	Screen * m_screen;
+};
+
+class IPiece : public Piece
+{
+public:
+	IPiece(Screen * scr, char piece[][]);
 };
 
 #endif
