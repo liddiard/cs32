937ed1c9d3c0067a128fc2a0133d5f4c7f9b3e75
omniaura <omniaura5@gmail.com>
2014-07-30 18:51:55 -0700

    created a file for globals, fixed Piece display method declaration

--- proj/3/Game.cpp
+++ proj/3/Game.cpp
@@ -1,34 +1,9 @@
+#include "globals.h"
 #include "Game.h"
 #include "Tank.h"
 #include "UserInterface.h"
 #include <string>
 
-const int SCREEN_WIDTH = 80;
-const int SCREEN_HEIGHT = 25;
-
-const int TANK_X = 0;
-const int TANK_Y = 0;
-
-const int TANK_WIDTH = 10;
-const int TANK_HEIGHT = 18;
-
-const int PROMPT_Y = 20;
-const int PROMPT_X = 0;
-
-const int SCORE_X = 16;
-const int SCORE_Y = 8;
-
-const int ROWS_LEFT_X = 16;
-const int ROWS_LEFT_Y = 9;
-
-const int LEVEL_X = 16;
-const int LEVEL_Y = 10;
-
-const int NEXT_PIECE_TITLE_X = 16;
-const int NEXT_PIECE_TITLE_Y = 3;
-
-const int NEXT_PIECE_X = 16;
-const int NEXT_PIECE_Y = 4;
 
 Game::Game(int width, int height)
  : m_screen(SCREEN_WIDTH, SCREEN_HEIGHT), m_level(1),
--- proj/3/Piece.h
+++ proj/3/Piece.h
@@ -1,13 +1,20 @@
+#ifndef PIECE_INCLUDED
+#define PIECE_INCLUDED
+
+#include "globals.h"
+
 class Piece
 {
 	public:
 		Piece();
-		virtual void display();
+		virtual void displayAtPosition(int x, int y);
 		virtual void fallOne();
 		virtual void fallAll();
 		virtual void rotateClockwise();
 		virtual void rotateCounterclockwise();
 
 	private:
-		char m_piece[4][4];
-}
+		char m_piece[PIECE_HEIGHT][PIECE_WIDTH];
+};
+
+#endif
