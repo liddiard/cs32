5a7516bea5cd8119aa6bc56a7fcb349b7f681b25
omniaura <omniaura5@gmail.com>
2014-07-30 22:11:37 -0700

    added globals file

---
+++ proj/3/globals.h
@@ -0,0 +1,34 @@
+#ifndef GLOBALS_INCLUDED
+#define GLOBALS_INCLUDED
+
+const int SCREEN_WIDTH = 80;
+const int SCREEN_HEIGHT = 25;
+
+const int TANK_X = 0;
+const int TANK_Y = 0;
+
+const int TANK_WIDTH = 10;
+const int TANK_HEIGHT = 18;
+
+const int PROMPT_Y = 20;
+const int PROMPT_X = 0;
+
+const int SCORE_X = 16;
+const int SCORE_Y = 8;
+
+const int ROWS_LEFT_X = 16;
+const int ROWS_LEFT_Y = 9;
+
+const int LEVEL_X = 16;
+const int LEVEL_Y = 10;
+
+const int PIECE_WIDTH = 4;
+const int PIECE_HEIGHT = 4;
+
+const int NEXT_PIECE_TITLE_X = 16;
+const int NEXT_PIECE_TITLE_Y = 3;
+
+const int NEXT_PIECE_X = 16;
+const int NEXT_PIECE_Y = 4;
+
+#endif
