1a171f526596d1d7a2f8bc642cd8c611cead96b5
omniaura <omniaura5@gmail.com>
2014-08-01 09:57:25 -0700

    made Tank offset an attribute

--- proj/3/Tank.cpp
+++ proj/3/Tank.cpp
@@ -5,30 +5,37 @@ Tank::Tank(int width, int height)
 {
 	m_width = width;
 	m_height = height;
+    m_x_offset = 0;
+    m_y_offset = 0;
 }
 
-void Tank::display(Screen& screen, int x_offset, int y_offset)
+void Tank::display(Screen& screen)
 {
 	const char TANK_CHAR = '@';
 	// left wall of tank
-	for (int i = y_offset; i < (y_offset + m_height); i++)
+	for (int i = m_y_offset; i < (m_y_offset + m_height); i++)
 	{
-		screen.gotoXY(x_offset, i);
+		screen.gotoXY(m_x_offset, i);
 		screen.printChar(TANK_CHAR);
 	}
 	// right wall of tank
-	for (int i = y_offset; i < (y_offset + m_height); i++)
+	for (int i = m_y_offset; i < (m_y_offset + m_height); i++)
 	{
-		screen.gotoXY((x_offset + m_width + 1), i); // "+ 1" gives the tank a *capacity* of speficied width, 
+		screen.gotoXY((m_x_offset + m_width + 1), i); // "+ 1" gives the tank a *capacity* of speficied width, 
 													// as opposed to putting a wall at the specified width
 		screen.printChar(TANK_CHAR);
 	}
 	// bottom of tank
-	for (int i = x_offset; i < (x_offset + m_width + 2); i++) // "+ 2 " to width accounts for combined thickness 
+	for (int i = m_x_offset; i < (m_x_offset + m_width + 2); i++) // "+ 2 " to width accounts for combined thickness 
 															  // of the two walls (one column each)
 	{
-		screen.gotoXY(i, (y_offset + m_height));
+		screen.gotoXY(i, (m_y_offset + m_height));
 		screen.printChar(TANK_CHAR);
 	}
 	screen.refresh();
+}
+
+void Tank::redrawContents(Screen& screen)
+{
+
 }
--- proj/3/Tank.h
+++ proj/3/Tank.h
@@ -1,17 +1,23 @@
 #ifndef TANK_INCLUDED
 #define TANK_INCLUDED
 
+#include <vector>
+
 class Screen;
 
 class Tank
 {
   public:
   	Tank(int width, int height);
-    void display(Screen& screen, int x_offset, int y_offset);
+    void display(Screen& screen);
+    void redrawContents(Screen& screen);
 
   private:
     int m_width;
     int m_height;
+    int m_x_offset;
+    int m_y_offset;
+    vector<Piece *> pieces;
 };
 
 #endif // TANK_INCLUDED
